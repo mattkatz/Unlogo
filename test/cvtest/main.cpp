77a87729c830455b0e7f64589759e4bc35aa67b3
Jeff Crouse <jefftimesten@gmail.com>
2010-08-15 15:40:37 -0400

    Added tests.  Moved to OpenCV2.1.  Next commit will be moving the filter to OpenCV2.1

--- test/cvtest/main.cpp
+++ test/cvtest/main.cpp
@@ -1,36 +1,41 @@
-#include <iostream>
 #include <cv.h>
 #include <cvaux.h>
 #include <highgui.h>
-#include <stdio.h>
-#include <ostream>
 
-using namespace std;
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
 using namespace cv;
 
-int				g_slider_position = 0;
-CvCapture*		g_capture         = NULL;
-CvMemStorage*	g_storage  = NULL;
-void			onTrackbarSlide(int pos);
-IplImage*		doCanny(IplImage* in, double lowThresh, double highThresh, double aperture);
-
-
-int main (int argc, char * const argv[]) {
+int main(int argc, char** argv)
+{
+    const char* object_filename = argc > 1 ? argv[1] : "box.png";
+    const char* scene_filename = argc > 2 ? argv[2] : "box_in_scene.png";
+    int i;
+    
+    cvNamedWindow("Object", 1);
+    cvNamedWindow("Image", 1);
+    cvNamedWindow("Object Correspondence", 1);
+    
+    Mat object = imread( object_filename, CV_LOAD_IMAGE_GRAYSCALE );
+    Mat image;
+    
+    double imgscale = 1;
 	
-	g_storage = cvCreateMemStorage(0); 
-	
-	const char* vid_win_name = "Video";
-	const char* object_win_name = "Template";
-	
-	// Show the template
-	const char* object_filename = argv[1];
-	Mat object = imread( object_filename );
-	cvNamedWindow( object_win_name, CV_WINDOW_AUTOSIZE );
-	imshow(object_win_name, object );
+    Mat _image = imread( scene_filename, CV_LOAD_IMAGE_GRAYSCALE );
+    resize(_image, image, Size(), 1./imgscale, 1./imgscale, INTER_CUBIC);
 	
 	
-	// Load the keypoint data for the template
-	Size patchSize(32, 32);
+    if( !object.data || !image.data )
+    {
+        fprintf( stderr, "Can not load %s and/or %s\n"
+                "Usage: find_obj [<object_filename> <scene_filename>]\n",
+                object_filename, scene_filename );
+        exit(-1);
+    }
+	
+    Size patchSize(32, 32);
     LDetector ldetector(7, 20, 2, 2000, patchSize.width, 2);
     ldetector.setVerbose(true);
     PlanarObjectDetector detector;
@@ -39,8 +44,9 @@ int main (int argc, char * const argv[]) {
     int blurKSize = 3;
     double sigma = 0;
     GaussianBlur(object, object, Size(blurKSize, blurKSize), sigma, sigma);
+    GaussianBlur(image, image, Size(blurKSize, blurKSize), sigma, sigma);
     buildPyramid(object, objpyr, ldetector.nOctaves-1);
-
+    buildPyramid(image, imgpyr, ldetector.nOctaves-1);
     
     vector<KeyPoint> objKeypoints, imgKeypoints;
 	PatchGenerator gen(0,256,5,true,0.8,1.2,-CV_PI/2,CV_PI/2,-CV_PI/2,CV_PI/2);
@@ -67,122 +73,67 @@ int main (int argc, char * const argv[]) {
         if( fs.open(model_filename, FileStorage::WRITE) )
             detector.write(fs, "ferns_model");
     }
-	printf("Now find the keypoints in the image, try recognize them and compute the homography matrix\n");
+    printf("Now find the keypoints in the image, try recognize them and compute the homography matrix\n");
     fs.release();
 	
-
+    vector<Point2f> dst_corners;
+    Mat correspond( object.rows + image.rows, std::max(object.cols, image.cols), CV_8UC3);
+    correspond = Scalar(0.);
+    Mat part(correspond, Rect(0, 0, object.cols, object.rows));
+    cvtColor(object, part, CV_GRAY2BGR);
+    part = Mat(correspond, Rect(0, object.rows, image.cols, image.rows));
+    cvtColor(image, part, CV_GRAY2BGR);
 	
-	// Take care of loading the video
-	cvNamedWindow( vid_win_name, CV_WINDOW_AUTOSIZE );
-    g_capture = cvCreateFileCapture( argv[2] );
+    vector<int> pairs;
+    Mat H;
     
-	
-	int frames = (int) cvGetCaptureProperty(g_capture, CV_CAP_PROP_FRAME_COUNT);
-    int tmpw = (int) cvGetCaptureProperty(g_capture, CV_CAP_PROP_FRAME_WIDTH);
-    int tmph = (int) cvGetCaptureProperty(g_capture, CV_CAP_PROP_FRAME_HEIGHT);
-
-    printf("opencv frames %d w %d h %d\n",frames,tmpw,tmph);
-
-	cvCreateTrackbar("Position", vid_win_name, &g_slider_position, frames, onTrackbarSlide);
-    Mat frame = cvQueryFrame( g_capture );
-	Mat gray_frame, thresh_frame, contours_frame, canny_frame, last_frame, rgb_tmp, composite;
-
-	CvSeq* contours = 0;
-
-	
-	VideoCapture cap(0); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
-        return -1;
-	
-    Mat edges;
-    namedWindow("edges",1);
-    for(;;)
+    double t = (double)getTickCount();
+    objKeypoints = detector.getModelPoints();
+    ldetector(imgpyr, imgKeypoints, 300);
+    
+    std::cout << "Object keypoints: " << objKeypoints.size() << "\n";
+    std::cout << "Image keypoints: " << imgKeypoints.size() << "\n";
+    bool found = detector(imgpyr, imgKeypoints, H, dst_corners, &pairs);
+    t = (double)getTickCount() - t;
+    printf("%gms\n", t*1000/getTickFrequency());
+    
+    if( found )
     {
-        Mat frame;
-        cap >> frame; // get a new frame from camera
-        cvtColor(frame, edges, CV_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
+        for( i = 0; i < 4; i++ )
+        {
+            Point r1 = dst_corners[i%4];
+            Point r2 = dst_corners[(i+1)%4];
+            line( correspond, Point(r1.x, r1.y+object.rows),
+                 Point(r2.x, r2.y+object.rows), Scalar(0,0,255) );
+        }
     }
-	
-	/*
-	
-	while(1)
-	{
-		if( !(frame = cvQueryFrame( g_capture )) ) break;
-		
-		// Make a gray version of the frame
-		cvCvtColor(frame, gray_frame ,CV_BGR2GRAY);
-		
-
-		GaussianBlur(frame, frame, Size(blurKSize, blurKSize), sigma, sigma);
-		buildPyramid(frame, frame, ldetector.nOctaves-1);
-		
-		
-		// Make the threshold version
-		cvThreshold(gray_frame, thresh_frame, 80, 255, CV_THRESH_BINARY);
-		
-		// Draw the contours onto the frame
-		cvFindContours( thresh_frame, g_storage, &contours );
-		cvCopy(frame, contours_frame);
-		if( contours )
-		{
-			cvDrawContours(contours_frame, contours, cvScalar(100, 200, 50), cvScalar(60, 20, 200), 100);
-		}
-
-		// Make the canny image
-		canny_frame = doCanny(gray_frame, 10, 100, 3);
-		
-		
-		// Copy the contoursframe into the composite image
-		cvSetImageROI(composite, cvRect(0, 0, frame->width, frame->height));
-		cvCopy(contours_frame, composite);
-		
-		
-		// Copy the threshold image into the composite
-		cvSetImageROI(composite, cvRect(frame->width, 0, frame->width, frame->height));
-		cvCvtColor(thresh_frame, rgb_tmp, CV_GRAY2BGR);
-		cvCopy(rgb_tmp, composite);
-		
-		// Copy the Canny image into the composite
-		cvSetImageROI(composite, cvRect(0, frame->height, frame->width, frame->height));
-		cvCvtColor(canny_frame, rgb_tmp, CV_GRAY2BGR);
-		cvCopy(rgb_tmp, composite);
-		
-		
-		cvResetImageROI(composite);
-		// Interface stuff
-       
-		frames++;
-		last_frame = cvCloneImage(frame);
-        cvSetTrackbarPos("Position",vid_win_name,frames);
-        cvShowImage( vid_win_name, composite );
-        char c = (char)cvWaitKey(0);
-        if( c == 27 ) break;
+    
+    for( i = 0; i < (int)pairs.size(); i += 2 )
+    {
+        line( correspond, objKeypoints[pairs[i]].pt,
+             imgKeypoints[pairs[i+1]].pt + Point2f(0,object.rows),
+             Scalar(0,255,0) );
     }
-	
-	cvClearMemStorage( g_storage );
-    cvReleaseCapture( &g_capture );
-    cvDestroyWindow( vid_win_name );
-	 */
-    return(0);
+    
+    imshow( "Object Correspondence", correspond );
+    Mat objectColor;
+    cvtColor(object, objectColor, CV_GRAY2BGR);
+    for( i = 0; i < (int)objKeypoints.size(); i++ )
+    {
+        circle( objectColor, objKeypoints[i].pt, 2, Scalar(0,0,255), -1 );
+        circle( objectColor, objKeypoints[i].pt, (1 << objKeypoints[i].octave)*15, Scalar(0,255,0), 1 );
+    }
+    Mat imageColor;
+    cvtColor(image, imageColor, CV_GRAY2BGR);
+    for( i = 0; i < (int)imgKeypoints.size(); i++ )
+    {
+        circle( imageColor, imgKeypoints[i].pt, 2, Scalar(0,0,255), -1 );
+        circle( imageColor, imgKeypoints[i].pt, (1 << imgKeypoints[i].octave)*15, Scalar(0,255,0), 1 );
+    }
+    imwrite("correspond.png", correspond );
+    imshow( "Object", objectColor );
+    imshow( "Image", imageColor );
+    
+    waitKey(0);
+    return 0;
 }
-
-void onTrackbarSlide(int pos)
-{
-    cvSetCaptureProperty(g_capture, CV_CAP_PROP_POS_FRAMES, pos);
-}
-
-IplImage* doCanny(IplImage* in, double lowThresh, double highThresh,  double aperture)
-{
-    if (in->nChannels != 1)
-        return(0); // Canny only handles gray scale images
-    IplImage* out = cvCreateImage( 
-								  cvGetSize( in ),
-								  in->depth, //IPL_DEPTH_8U,    
-								  1);
-    cvCanny( in, out, lowThresh, highThresh, aperture );
-    return( out );
-};
