0e9c36a0b2d632dc53a71aa8074ad4447e44e821
Jeff Crouse <jefftimesten@gmail.com>
2010-08-15 20:19:43 -0400

    added optical flow to movietest

--- test/movietest/main.cpp
+++ test/movietest/main.cpp
@@ -1,4 +1,5 @@
 #include <highgui.h>
+#include <cvaux.h>
 #include "opencv2/core/core.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -8,10 +9,102 @@
 using namespace cv;
 using namespace std;
 
+const string winName = "correspondences";
+
 
 #define DRAW_RICH_KEYPOINTS_MODE     0
 #define DRAW_OUTLIERS_MODE           0
 
+//void warpPerspectiveRand( const Mat& src, Mat& dst, Mat& H, RNG& rng );
+void doIteration( const Mat& img1, Mat& img2, Mat& lastframe,
+				 vector<KeyPoint>& keypoints1, const Mat& descriptors1,
+				 Ptr<FeatureDetector>& detector, Ptr<DescriptorExtractor>& descriptorExtractor,
+				 Ptr<DescriptorMatcher>& descriptorMatcher,
+				 double ransacReprojThreshold);
+
+// Detector Types: FAST, STAR, SIFT, SURF, MSER, GFTT, HARRIS
+// Descriptor Types: SIFT, SURF
+
+
+int main (int argc, char * const argv[]) {
+
+	cout << "< Creating detector, descriptor extractor and descriptor matcher ..." << endl;
+    Ptr<FeatureDetector> detector = createDetector( argv[1] );
+    Ptr<DescriptorExtractor> descriptorExtractor = createDescriptorExtractor( argv[2] );
+    Ptr<DescriptorMatcher> descriptorMatcher = createDescriptorMatcher( "BruteForce" );
+    cout << ">" << endl;
+    if( detector.empty() || descriptorExtractor.empty() || descriptorMatcher.empty()  )
+    {
+        cout << "Can not create detector or descriptor exstractor or descriptor matcher of given types" << endl;
+        return -1;
+	}
+	
+	cout << "< Reading the template..." << endl;
+    Mat img1 = imread( argv[3], CV_LOAD_IMAGE_GRAYSCALE), img2;
+	
+	if( img1.empty() )
+    {
+        cout << "Can not read template image" << endl;
+        return -1;
+    }
+	
+	vector<KeyPoint> keypoints1;
+    detector->detect( img1, keypoints1 );
+    cout << keypoints1.size() << " points" << endl << ">" << endl;
+	
+    cout << "< Computing descriptors for keypoints from first image..." << endl;
+    Mat descriptors1;
+    descriptorExtractor->compute( img1, keypoints1, descriptors1 );
+    cout << ">" << endl;
+	
+	// If srcPoints and dstPoints are measured in pixels, 
+	// it usually makes sense to set this parameter somewhere in the range 1 to 10.
+	int ransacReprojThreshold = 1;
+
+	
+	
+	VideoCapture cap(argv[4]); // open the default camera
+    if(!cap.isOpened())  // check if we succeeded
+        return -1;
+	
+	Mat frame;
+	Mat bwframe;
+	Mat lastframe;
+	//RNG rng = theRNG();
+	
+    for(int i=0; 1; i++)
+    {
+        cap >> frame; // get a new frame from camera
+		cvtColor(frame, bwframe, CV_BGR2GRAY);
+		
+		
+		if(i==0) {
+			lastframe = bwframe;
+		}
+		
+		
+		doIteration( img1, bwframe, lastframe, keypoints1, descriptors1,
+					detector, descriptorExtractor, descriptorMatcher,
+					ransacReprojThreshold );
+		
+		/*
+        cvtColor(frame, edges, CV_BGR2GRAY);
+        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        Canny(edges, edges, 0, 30, 3);
+		 imshow("edges", frame);
+		 */
+		
+		lastframe = bwframe.clone();
+        if(waitKey(30) >= 0) break;
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+	
+
+}
+
+
+/*
 void warpPerspectiveRand( const Mat& src, Mat& dst, Mat& H, RNG& rng )
 {
     H.create(3, 3, CV_32FC1);
@@ -27,21 +120,18 @@ void warpPerspectiveRand( const Mat& src, Mat& dst, Mat& H, RNG& rng )
 	
     warpPerspective( src, dst, H, src.size() );
 }
+*/
 
-const string winName = "correspondences";
-
-void doIteration( const Mat& img1, Mat& img2, bool isWarpPerspective,
+void doIteration( const Mat& img1, Mat& img2, Mat& lastframe,
 				 vector<KeyPoint>& keypoints1, const Mat& descriptors1,
 				 Ptr<FeatureDetector>& detector, Ptr<DescriptorExtractor>& descriptorExtractor,
 				 Ptr<DescriptorMatcher>& descriptorMatcher,
-				 double ransacReprojThreshold, RNG& rng )
+				 double ransacReprojThreshold )
 {
     assert( !img1.empty() );
     Mat H12;
-    if( isWarpPerspective )
-        warpPerspectiveRand(img1, img2, H12, rng );
-    else
-        assert( !img2.empty()/* && img2.cols==img1.cols && img2.rows==img1.rows*/ );
+
+	assert( !img2.empty()/* && img2.cols==img1.cols && img2.rows==img1.rows*/ );
 	
     cout << endl << "< Extracting keypoints from second image..." << endl;
     vector<KeyPoint> keypoints2;
@@ -82,7 +172,7 @@ void doIteration( const Mat& img1, Mat& img2, bool isWarpPerspective,
         cout << ">" << endl;
     }
 	
-    if( !isWarpPerspective && ransacReprojThreshold >= 0 )
+    if(ransacReprojThreshold >= 0 )
     {
         cout << "< Computing homography (RANSAC)..." << endl;
         vector<Point2f> points1; KeyPoint::convert(keypoints1, points1);
@@ -92,7 +182,7 @@ void doIteration( const Mat& img1, Mat& img2, bool isWarpPerspective,
     }
 	
     Mat drawImg;
-
+	
     if( !H12.empty() ) // filter outliers
     {
         vector<char> matchesMask( matches.size(), 0 );
@@ -123,78 +213,31 @@ void doIteration( const Mat& img1, Mat& img2, bool isWarpPerspective,
     else
         drawMatches( img1, keypoints1, img2, keypoints2, matches, drawImg );
 	
-    imshow( winName, drawImg );
-}
-
-
-
-int main (int argc, char * const argv[]) {
-
-	cout << "< Creating detector, descriptor extractor and descriptor matcher ..." << endl;
-    Ptr<FeatureDetector> detector = createDetector( argv[1] );
-    Ptr<DescriptorExtractor> descriptorExtractor = createDescriptorExtractor( argv[2] );
-    Ptr<DescriptorMatcher> descriptorMatcher = createDescriptorMatcher( "BruteForce" );
-    cout << ">" << endl;
-    if( detector.empty() || descriptorExtractor.empty() || descriptorMatcher.empty()  )
-    {
-        cout << "Can not create detector or descriptor exstractor or descriptor matcher of given types" << endl;
-        return -1;
+	
+	imshow( winName, drawImg );
+	
+	
+	
+	// Jeff's Optical Flow Stuff
+	
+	Mat flow;
+	cvtColor(img2, flow, CV_GRAY2RGB);
+	
+	vector<Point2f> prevPts;
+	vector<Point2f> nextPts;
+	for(int i=0; i<keypoints2.size(); i++) {
+		prevPts.push_back( keypoints2[i].pt );
 	}
 	
-	cout << "< Reading the template..." << endl;
-    Mat img1 = imread( argv[3], CV_LOAD_IMAGE_GRAYSCALE), img2;
+	vector<uchar> status;
+	vector<float> err;
 	
-	if( img1.empty() )
-    {
-        cout << "Can not read template image" << endl;
-        return -1;
-    }
-	
-	vector<KeyPoint> keypoints1;
-    detector->detect( img1, keypoints1 );
-    cout << keypoints1.size() << " points" << endl << ">" << endl;
-	
-    cout << "< Computing descriptors for keypoints from first image..." << endl;
-    Mat descriptors1;
-    descriptorExtractor->compute( img1, keypoints1, descriptors1 );
-    cout << ">" << endl;
-	
-	
-	int ransacReprojThreshold = 10;
-	bool isWarpPerspective = false;
-	
-	
-	VideoCapture cap(argv[4]); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
-        return -1;
-	
-    Mat edges;
-	RNG rng = theRNG();
-	
-    for(;;)
-    {
-        Mat frame;
-		Mat bwframe;
-		
-        cap >> frame; // get a new frame from camera
-		
-		cvtColor(frame, bwframe, CV_BGR2GRAY);
-		
-		doIteration( img1, bwframe, isWarpPerspective, keypoints1, descriptors1,
-					detector, descriptorExtractor, descriptorMatcher,
-					ransacReprojThreshold, rng );
-		
-		/*
-        cvtColor(frame, edges, CV_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
-		 imshow("edges", frame);
-		 */
-		
-        if(waitKey(30) >= 0) break;
-    }
-    // the camera will be deinitialized automatically in VideoCapture destructor
-    return 0;
-	
+	calcOpticalFlowPyrLK(lastframe, img2, prevPts, nextPts, status, err);
 
+	for(int i=0; i<prevPts.size(); i++) {
+		circle(flow, prevPts[i], 4, CV_RGB(255, 0, 0));
+		line(flow, prevPts[i], nextPts[i],  CV_RGB(200, 0, 255));
+	}
+    
+	imshow( "optflow", flow );
 }
