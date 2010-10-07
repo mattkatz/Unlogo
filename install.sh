6120c4352abbaf1fdb22e6041c88273fc60ad879
Jeff Crouse <jefftimesten@gmail.com>
2010-10-07 15:21:53 -0400

    changed libpng download location

--- install.sh
+++ install.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+set -e
 set -u
+set -o pipefail
 SCRIPT=$(cd ${0%/*} && echo $PWD/${0##*/})
 PREFIX=`dirname $SCRIPT`
 
 clear
 echo -------------------------
-echo "Welcome to the OpenFramer Installer"
+echo "Welcome to the Framehack Installer"
 echo "Install location: $PREFIX"
 echo -------------------------
 
 
 read -p "Press any key to start install"
 echo "Making the distribution directory..."
-mkdir dist
-cd dist
+mkdir -p $PREFIX/dist
+cd $PREFIX/dist
 
 
 
@@ -26,8 +28,9 @@ curl -O http://www.ece.uvic.ca/~mdadams/jasper/software/jasper-1.900.1.zip
 unzip jasper-1.900.1.zip
 cd jasper-1.900.1
 ./configure --prefix=$PREFIX
-make; make install
-cd ..
+make
+make install
+cd $PREFIX/dist
 
 
 
@@ -39,8 +42,9 @@ curl -O http://www.ijg.org/files/jpegsrc.v8b.tar.gz
 tar -xvf jpegsrc.v8b.tar.gz
 cd jpeg-8b
 ./configure --enable-shared=no --prefix=$PREFIX
-make; make install
-cd ..
+make
+make install
+cd $PREFIX/dist
 
 
 
@@ -48,12 +52,13 @@ clear
 echo -------------------------
 echo "Downloading and Building PNG"
 echo -------------------------
-curl -O http://downloads.sourceforge.net/project/libpng/01-libpng-master/1.4.4/libpng-1.4.4.tar.gz
+curl -O ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.4.4.tar.gz
 tar -xvf libpng-1.4.4.tar.gz
 cd libpng-1.4.4
 ./configure --enable-shared=no --prefix=$PREFIX
-make; make install
-cd ..
+make
+make install
+cd $PREFIX/dist
 
 
 
@@ -65,8 +70,9 @@ curl -O ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.9.4.tar.gz
 tar -xvf tiff-3.9.4.tar.gz
 cd tiff-3.9.4
 ./configure --enable-shared=no --prefix=$PREFIX
-make; make install
-cd ..
+make
+make install
+cd $PREFIX/dist
 
 
 
@@ -78,8 +84,9 @@ curl -O http://downloads.sourceforge.net/project/lame/lame/3.98.4/lame-3.98.4.ta
 tar -xvf lame-3.98.4.tar.gz
 cd lame-3.98.4
 ./configure --enable-shared=no --prefix=$PREFIX
-make; make install
-cd ..
+make
+make install
+cd $PREFIX/dist
 
 
 
@@ -89,11 +96,11 @@ echo "Checking out and Building FFMPEG"
 echo -------------------------
 svn co -r 25296 svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg 
 cd ffmpeg
-patch -p0 -i ../../share/patches/ffmpeg_framehack_rev25296.patch
+patch -p0 -i $PREFIX/share/patches/ffmpeg_framehack_rev25296.patch
 ./configure --arch=x86 --enable-libmp3lame --enable-libx264 --enable-nonfree --enable-gpl --disable-doc --prefix=$PREFIX
 make
 make install
-cd ..
+cd $PREFIX/dist
 
 
 
@@ -103,10 +110,11 @@ echo "Checking out and Building OpenCV"
 echo -------------------------
 svn co -r 3713 https://code.ros.org/svn/opencv/trunk/opencv 
 cd opencv
-patch -p0 -i ../../share/patches/opencv_framehack_rev3713.patch
+patch -p0 -i $PREFIX/share/patches/opencv_framehack_rev3713.patch
 cmake -G "Unix Makefiles" -D BUILD_TESTS=OFF -D BUILD_SHARED_LIBS=OFF -D CMAKE_BUILD_TYPE=RelWithDebInfo -D CMAKE_INSTALL_PREFIX=$PREFIX .
-make; make install
-cd ..
+make
+make install
+cd $PREFIX/dist
 
 
 
