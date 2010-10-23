04181b9c2976e8a56fa20c43605fa54290b570ec
Jeff Crouse <jefftimesten@gmail.com>
2010-10-23 17:48:13 -0400

    Changed install script to support both curl and wget

--- install.sh
+++ install.sh
@@ -13,6 +13,10 @@ export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 
+WGET=wget
+type -P wget &>/dev/null || { 
+WGET=curl -L -O
+}
 
 clear
 echo -------------------------
@@ -33,136 +37,126 @@ mkdir -p $PREFIX/dist
 cd $PREFIX/dist
 
 
-# 
-# clear
-# echo -------------------------
-# echo "Downloading and building LAME"
-# echo -------------------------
-# curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.98.4/lame-3.98.4.tar.gz
-# tar -xvf lame-3.98.4.tar.gz
-# cd lame-3.98.4
-# ./configure --prefix=$PREFIX -enable-shared=no
-# make && make install
-# cd $PREFIX/dist
-# 
-# 
-# clear
-# echo -------------------------
-# echo "Downloading and building YASM"
-# echo -------------------------
-# curl -L -O http://www.tortall.net/projects/yasm/releases/yasm-1.1.0.tar.gz
-# tar -xvf yasm-1.1.0.tar.gz
-# cd yasm-1.1.0
-# ./configure --prefix=$PREFIX
-# make && make install
-# cd $PREFIX/dist
-# 
-# 
-# clear
-# echo -------------------------
-# echo "Downloading and building FAAC"
-# echo -------------------------
-# curl -L -O http://downloads.sourceforge.net/faac/faac-1.28.tar.gz
-# tar -xvf faac-1.28.tar.gz
-# cd faac-1.28
-# ./configure --prefix=$PREFIX --enable-shared=no
-# make && make install
-# cd $PREFIX/dist
-# 
-# 
-# clear
-# echo -------------------------
-# echo "Downloading and building opencore-arm"
-# echo -------------------------
-# curl -L -O http://downloads.sourceforge.net/project/opencore-amr/opencore-amr/0.1.2/opencore-amr-0.1.2.tar.gz
-# tar -xvf opencore-amr-0.1.2.tar.gz
-# cd opencore-amr-0.1.2
-# ./configure --enable-shared=no --prefix=$PREFIX
-# make && make install clean
-# cd $PREFIX/dist
-# 
-# 
-# clear
-# echo -------------------------
-# echo "Downloading and building libogg"
-# echo -------------------------
-# curl -L -O http://downloads.xiph.org/releases/ogg/libogg-1.2.0.tar.gz
-# tar -xvf libogg-1.2.0.tar.gz
-# cd libogg-1.2.0
-# ./configure --enable-shared=no --prefix=$PREFIX
-# make && make install clean
-# cd $PREFIX/dist
-# 
-# 
-# clear
-# echo -------------------------
-# echo "Downloading and building libvorbis"
-# echo -------------------------
-# curl -L -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.1.tar.gz
-# tar -xvf libvorbis-1.3.1.tar.gz
-# cd libvorbis-1.3.1
-# ./configure --enable-shared=no --prefix=$PREFIX
-# make && make install clean
-# cd $PREFIX/dist
-# 
-# 
-# clear
-# echo -------------------------
-# echo "Downloading and building libtheora"
-# echo -------------------------
-# curl -L -O http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2
-# tar -xvf libtheora-1.1.1.tar.bz2
-# cd libtheora-1.1.1
-# ./configure --enable-shared=no --prefix=$PREFIX --with-ogg=$PREFIX --with-vorbis=$PREFIX
-# make && make install clean
-# cd $PREFIX/dist
-# 
-# 
-# clear
-# echo -------------------------
-# echo "Checking out and building x264"
-# echo -------------------------
-# curl -L -O ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20100922-2245.tar.bz2
-# tar -xvf x264-snapshot-20100922-2245.tar.bz2
-# cd x264-snapshot-20100922-2245
-# ./configure --prefix=$PREFIX
-# make && make install clean
-# cd $PREFIX/dist
-# 
-# 
-# clear
-# echo -------------------------
-# echo "Checking out and building xvidcore"
-# echo -------------------------
-# curl -L -O http://downloads.xvid.org/downloads/xvidcore-1.2.2.tar.gz
-# tar -xvf xvidcore-1.2.2.tar.gz
-# cd xvidcore/build/generic
-# ./configure --prefix=$PREFIX
-# make && make install clean
-# cd $PREFIX/dist
-# 
-# 
-# clear
-# echo -------------------------
-# echo "Checking out and building FFMPEG"
-# echo -------------------------
-# svn co -r 25296 svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg 
-# cd ffmpeg
-# patch -p0 -i $PREFIX/share/patches/ffmpeg_framehack_rev25296.patch
-# ./configure --prefix=$PREFIX --disable-stripping --enable-debug --enable-gpl --disable-doc --enable-libmp3lame --enable-libx264 --enable-nonfree --enable-libvorbis --enable-libxvid --enable-version3 --enable-pthreads --enable-libfaac --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libtheora --enable-libxvid --enable-x11grab
-# make && make install
-# cd $PREFIX/dist
+
+clear
+echo -------------------------
+echo "Downloading and building LAME"
+echo -------------------------
+${WGET} http://downloads.sourceforge.net/project/lame/lame/3.98.4/lame-3.98.4.tar.gz
+tar -xvf lame-3.98.4.tar.gz
+cd lame-3.98.4
+./configure --prefix=$PREFIX -enable-shared=no
+make && make install
+cd $PREFIX/dist
 
 
 clear
- echo -------------------------
- echo "Building and installing Framehack"
- echo -------------------------
- cd framehack
- ./configure --prefix=$PREFIX
- make
- make install
- cd $PREFIX/dist
+echo -------------------------
+echo "Downloading and building YASM"
+echo -------------------------
+${WGET} http://www.tortall.net/projects/yasm/releases/yasm-1.1.0.tar.gz
+tar -xvf yasm-1.1.0.tar.gz
+cd yasm-1.1.0
+./configure --prefix=$PREFIX
+make && make install
+cd $PREFIX/dist
+
+
+clear
+echo -------------------------
+echo "Downloading and building FAAC"
+echo -------------------------
+${WGET} http://downloads.sourceforge.net/faac/faac-1.28.tar.gz
+tar -xvf faac-1.28.tar.gz
+cd faac-1.28
+./configure --prefix=$PREFIX --enable-shared=no
+make && make install
+cd $PREFIX/dist
+
+
+clear
+echo -------------------------
+echo "Downloading and building opencore-arm"
+echo -------------------------
+${WGET} http://downloads.sourceforge.net/project/opencore-amr/opencore-amr/0.1.2/opencore-amr-0.1.2.tar.gz
+tar -xvf opencore-amr-0.1.2.tar.gz
+cd opencore-amr-0.1.2
+./configure --enable-shared=no --prefix=$PREFIX
+make && make install clean
+cd $PREFIX/dist
+
+
+clear
+echo -------------------------
+echo "Downloading and building libogg"
+echo -------------------------
+${WGET} http://downloads.xiph.org/releases/ogg/libogg-1.2.0.tar.gz
+tar -xvf libogg-1.2.0.tar.gz
+cd libogg-1.2.0
+./configure --enable-shared=no --prefix=$PREFIX
+make && make install clean
+cd $PREFIX/dist
+
+
+clear
+echo -------------------------
+echo "Downloading and building libvorbis"
+echo -------------------------
+${WGET} http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.1.tar.gz
+tar -xvf libvorbis-1.3.1.tar.gz
+cd libvorbis-1.3.1
+./configure --enable-shared=no --prefix=$PREFIX
+make && make install clean
+cd $PREFIX/dist
+
+
+clear
+echo -------------------------
+echo "Downloading and building libtheora"
+echo -------------------------
+${WGET} http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2
+tar -xvf libtheora-1.1.1.tar.bz2
+cd libtheora-1.1.1
+./configure --enable-shared=no --prefix=$PREFIX --with-ogg=$PREFIX --with-vorbis=$PREFIX
+make && make install clean
+cd $PREFIX/dist
+
+
+clear
+echo -------------------------
+echo "Checking out and building x264"
+echo -------------------------
+${WGET} ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20100922-2245.tar.bz2
+tar -xvf x264-snapshot-20100922-2245.tar.bz2
+cd x264-snapshot-20100922-2245
+./configure --prefix=$PREFIX
+make && make install clean
+cd $PREFIX/dist
+
+
+clear
+echo -------------------------
+echo "Checking out and building xvidcore"
+echo -------------------------
+${WGET} http://downloads.xvid.org/downloads/xvidcore-1.2.2.tar.gz
+tar -xvf xvidcore-1.2.2.tar.gz
+cd xvidcore/build/generic
+./configure --prefix=$PREFIX
+make && make install clean
+cd $PREFIX/dist
+
+
+clear
+echo -------------------------
+echo "Checking out and building FFMPEG"
+echo -------------------------
+svn co -r 25296 svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg 
+cd $PREFIX/dist/ffmpeg
+patch -p0 -i $PREFIX/share/patches/ffmpeg_framehack_rev25296.patch
+./configure --prefix=$PREFIX --disable-asm --disable-stripping --enable-debug --enable-gpl --disable-doc --enable-libmp3lame --enable-libx264 --enable-nonfree --enable-libvorbis --enable-libxvid --enable-version3 --enable-pthreads --enable-libfaac --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libtheora --enable-libxvid --enable-x11grab
+make && make install
+cd $PREFIX/dist
+
 
 #clear
 # echo -------------------------
@@ -207,7 +201,7 @@ clear
 # echo -------------------------
 # echo "Downloading and building TIFF"
 # echo -------------------------
-# curl -L -O ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.9.4.tar.gz
+# ${WGET} ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.9.4.tar.gz
 # tar -xvf tiff-3.9.4.tar.gz
 # cd tiff-3.9.4
 # ./configure --enable-shared=no --prefix=$PREFIX
@@ -221,7 +215,7 @@ clear
 echo -------------------------
 echo "Checking out and building CMake"
 echo -------------------------
-curl -L -O http://www.cmake.org/files/v2.8/cmake-2.8.2.tar.gz
+${WGET} http://www.cmake.org/files/v2.8/cmake-2.8.2.tar.gz
 tar -xvf cmake-2.8.2.tar.gz
 cd cmake-2.8.2
 ./configure --prefix=$PREFIX
@@ -250,16 +244,16 @@ export CFLAGS=""
 export CPPFLAGS=""
 export LDFLAGS=""
 
-# clear
-# echo -------------------------
-# echo "Checking out and building OpenCV"
-# echo -------------------------
-# svn -r 3713 co https://code.ros.org/svn/opencv/trunk/opencv opencv2.1
-# cd opencv2.1
-# patch -p0 -i $PREFIX/share/patches/opencv_framehack_rev3713.patch
-# cmake -G "Unix Makefiles" -D OPENCV_BUILD_3RDPARTY_LIBS=FALSE -D BUILD_EXAMPLES=OFF -D BUILD_NEW_PYTHON_SUPPORT=OFF -D BUILD_TESTS=OFF -D BUILD_SHARED_LIBS=OFF -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=$PREFIX .
-# make && make install
-# cd $PREFIX/dist
+clear
+echo -------------------------
+echo "Checking out and building OpenCV"
+echo -------------------------
+svn -r 3713 co https://code.ros.org/svn/opencv/trunk/opencv opencv2.1
+cd opencv2.1
+patch -p0 -i $PREFIX/share/patches/opencv_framehack_rev3713.patch
+cmake -G "Unix Makefiles" -D OPENCV_BUILD_3RDPARTY_LIBS=FALSE -D BUILD_EXAMPLES=OFF -D BUILD_NEW_PYTHON_SUPPORT=OFF -D BUILD_TESTS=OFF -D BUILD_SHARED_LIBS=OFF -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=$PREFIX .
+make && make install
+cd $PREFIX/dist
 
 
 
