764309a0a9546a3543c5dbd1fa424eb801dd57be
Jeff Crouse <jefftimesten@gmail.com>
2011-02-24 18:52:55 -0500

    Another batch of uncommitted changes from SVN

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ WGET=curl -L -O
 
 clear
 echo -------------------------
-echo "Welcome to the Framehack Installer"
+echo "Welcome to the Unlogo Requirement Installer"
 echo "Framehack will be installed into $PREFIX"
 echo "PATH $PATH"
 echo "LD_LIBRARY_PATH $LD_LIBRARY_PATH"
@@ -34,193 +34,184 @@ echo -------------------------
 read -p "Press any key to start install"
 echo "Making the distribution directory..."
 mkdir -p $PREFIX/dist
-cd $PREFIX/dist
 
 
 
-clear
-echo -------------------------
-echo "Downloading and building LAME"
-echo -------------------------
-${WGET} http://downloads.sourceforge.net/project/lame/lame/3.98.4/lame-3.98.4.tar.gz
-tar -xvf lame-3.98.4.tar.gz
-cd lame-3.98.4
-./configure --prefix=$PREFIX -enable-shared=no
-make && make install
-cd $PREFIX/dist
+if [ -e "$PREFIX/lib/libmp3lame.a" ]
+then
+	echo "LAME is already installed"
+else
+	clear
+	echo -------------------------
+	echo "Downloading and building LAME"
+	echo -------------------------
+	cd $PREFIX/dist
+	${WGET} http://downloads.sourceforge.net/project/lame/lame/3.98.4/lame-3.98.4.tar.gz
+	tar -xvf lame-3.98.4.tar.gz
+	cd lame-3.98.4
+	./configure --prefix=$PREFIX -enable-shared=no
+	make && make install
+fi
 
 
-clear
-echo -------------------------
-echo "Downloading and building YASM"
-echo -------------------------
-${WGET} http://www.tortall.net/projects/yasm/releases/yasm-1.1.0.tar.gz
-tar -xvf yasm-1.1.0.tar.gz
-cd yasm-1.1.0
-./configure --prefix=$PREFIX
-make && make install
-cd $PREFIX/dist
+
+if [ -e "$PREFIX/lib/libyasm.a" ]
+then
+	echo "YASM is already installed"
+else
+	clear
+	echo -------------------------
+	echo "Downloading and building YASM"
+	echo -------------------------
+	cd $PREFIX/dist
+	${WGET} http://www.tortall.net/projects/yasm/releases/yasm-1.1.0.tar.gz
+	tar -xvf yasm-1.1.0.tar.gz
+	cd yasm-1.1.0
+	./configure --prefix=$PREFIX
+	make && make install
+fi
 
 
-clear
-echo -------------------------
-echo "Downloading and building FAAC"
-echo -------------------------
-${WGET} http://downloads.sourceforge.net/faac/faac-1.28.tar.gz
-tar -xvf faac-1.28.tar.gz
-cd faac-1.28
-./configure --prefix=$PREFIX --enable-shared=no
-make && make install
-cd $PREFIX/dist
+
+if [ -e "$PREFIX/lib/libfaac.a" ]
+then
+	echo "FAAC is already installed"
+else
+	clear
+	echo -------------------------
+	echo "Downloading and building FAAC"
+	echo -------------------------
+	cd $PREFIX/dist
+	${WGET} http://downloads.sourceforge.net/faac/faac-1.28.tar.gz
+	tar -xvf faac-1.28.tar.gz
+	cd faac-1.28
+	./configure --prefix=$PREFIX --enable-shared=no
+	make && make install
+fi
 
 
-clear
-echo -------------------------
-echo "Downloading and building opencore-arm"
-echo -------------------------
-${WGET} http://downloads.sourceforge.net/project/opencore-amr/opencore-amr/0.1.2/opencore-amr-0.1.2.tar.gz
-tar -xvf opencore-amr-0.1.2.tar.gz
-cd opencore-amr-0.1.2
-./configure --enable-shared=no --prefix=$PREFIX
-make && make install clean
-cd $PREFIX/dist
+
+if [ -e "$PREFIX/lib/libopencore-amrnb.a" ]
+then
+	echo "opencore-arm is already installed"
+else
+	clear
+	echo -------------------------
+	echo "Downloading and building opencore-arm"
+	echo -------------------------
+	cd $PREFIX/dist
+	${WGET} http://downloads.sourceforge.net/project/opencore-amr/opencore-amr/0.1.2/opencore-amr-0.1.2.tar.gz
+	tar -xvf opencore-amr-0.1.2.tar.gz
+	cd opencore-amr-0.1.2
+	./configure --enable-shared=no --prefix=$PREFIX
+	make && make install clean
+fi
 
 
-clear
-echo -------------------------
-echo "Downloading and building libogg"
-echo -------------------------
-${WGET} http://downloads.xiph.org/releases/ogg/libogg-1.2.0.tar.gz
-tar -xvf libogg-1.2.0.tar.gz
-cd libogg-1.2.0
-./configure --enable-shared=no --prefix=$PREFIX
-make && make install clean
-cd $PREFIX/dist
 
 
-clear
-echo -------------------------
-echo "Downloading and building libvorbis"
-echo -------------------------
-${WGET} http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.1.tar.gz
-tar -xvf libvorbis-1.3.1.tar.gz
-cd libvorbis-1.3.1
-./configure --enable-shared=no --prefix=$PREFIX
-make && make install clean
-cd $PREFIX/dist
+if [ -e "$PREFIX/lib/libogg.a" ]
+then
+	echo "libogg is already installed"
+else
+	clear
+	echo -------------------------
+	echo "Downloading and building libogg"
+	echo -------------------------
+	cd $PREFIX/dist
+	${WGET} http://downloads.xiph.org/releases/ogg/libogg-1.2.0.tar.gz
+	tar -xvf libogg-1.2.0.tar.gz
+	cd libogg-1.2.0
+	./configure --enable-shared=no --prefix=$PREFIX
+	make && make install clean
+	cd $PREFIX/dist
+fi
 
 
-clear
-echo -------------------------
-echo "Downloading and building libtheora"
-echo -------------------------
-${WGET} http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2
-tar -xvf libtheora-1.1.1.tar.bz2
-cd libtheora-1.1.1
-./configure --enable-shared=no --prefix=$PREFIX --with-ogg=$PREFIX --with-vorbis=$PREFIX
-make && make install clean
-cd $PREFIX/dist
+
+if [ -e "$PREFIX/lib/libvorbis.a" ]
+then
+	echo "libvorbis is already installed"
+else
+	clear
+	echo -------------------------
+	echo "Downloading and building libvorbis"
+	echo -------------------------
+	${WGET} http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.1.tar.gz
+	tar -xvf libvorbis-1.3.1.tar.gz
+	cd libvorbis-1.3.1
+	./configure --enable-shared=no --prefix=$PREFIX
+	make && make install clean
+fi
 
 
-clear
-echo -------------------------
-echo "Checking out and building x264"
-echo -------------------------
-${WGET} ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20100922-2245.tar.bz2
-tar -xvf x264-snapshot-20100922-2245.tar.bz2
-cd x264-snapshot-20100922-2245
-./configure --prefix=$PREFIX
-make && make install clean
-cd $PREFIX/dist
+
+if [ -e "$PREFIX/lib/libtheora.a" ]
+then
+	echo "libvorbis is already installed"
+else
+	clear
+	echo -------------------------
+	echo "Downloading and building libtheora"
+	echo -------------------------
+	cd $PREFIX/dist
+	${WGET} http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2
+	tar -xvf libtheora-1.1.1.tar.bz2
+	cd libtheora-1.1.1
+	./configure --enable-shared=no --prefix=$PREFIX --with-ogg=$PREFIX --with-vorbis=$PREFIX
+	make && make install clean
+	cd $PREFIX/dist
+fi
 
 
-clear
-echo -------------------------
-echo "Checking out and building xvidcore"
-echo -------------------------
-${WGET} http://downloads.xvid.org/downloads/xvidcore-1.2.2.tar.gz
-tar -xvf xvidcore-1.2.2.tar.gz
-cd xvidcore/build/generic
-./configure --prefix=$PREFIX
-make && make install clean
-cd $PREFIX/dist
+
+if [ -e "$PREFIX/lib/libx264.a" ]
+then
+	echo "x264 is already installed"
+else
+	clear
+	echo -------------------------
+	echo "Downloading and building x264"
+	echo -------------------------
+	${WGET} ftp://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20100922-2245.tar.bz2
+	tar -xvf x264-snapshot-20100922-2245.tar.bz2
+	cd x264-snapshot-20100922-2245
+	./configure --prefix=$PREFIX
+	make && make install clean
+fi
 
 
-clear
-echo -------------------------
-echo "Checking out and building FFMPEG"
-echo -------------------------
-svn co -r 25296 svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg 
-cd $PREFIX/dist/ffmpeg
-patch -p0 -i $PREFIX/share/patches/ffmpeg_framehack_rev25296.patch
-./configure --prefix=$PREFIX --disable-asm --disable-stripping --enable-debug --enable-gpl --disable-doc --enable-libmp3lame --enable-libx264 --enable-nonfree --enable-libvorbis --enable-libxvid --enable-version3 --enable-pthreads --enable-libfaac --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libtheora --enable-libxvid --enable-x11grab
-make && make install
-cd $PREFIX/dist
+if [ -e "$PREFIX/lib/libxvidcore.a" ]
+then
+	echo "xvidcore is already installed"
+else
+	clear
+	echo -------------------------
+	echo "Downloading and building xvidcore"
+	echo -------------------------
+	cd $PREFIX/dist
+	${WGET} http://downloads.xvid.org/downloads/xvidcore-1.2.2.tar.gz
+	tar -xvf xvidcore-1.2.2.tar.gz
+	cd xvidcore/build/generic
+	./configure --prefix=$PREFIX
+	make && make install clean
+fi
 
 
-#clear
-# echo -------------------------
-# echo "Downloading and building Jasper"
-# echo -------------------------
-# curl -O http://www.ece.uvic.ca/~mdadams/jasper/software/jasper-1.900.1.zip
-# unzip jasper-1.900.1.zip
-# cd jasper-1.900.1
-# ./configure --prefix=$PREFIX
-# make
-# make install
-# cd $PREFIX/dist
-# 
-# 
-# clear
-# echo -------------------------
-# echo "Downloading and building JPEG"
-# echo -------------------------
-# curl -O http://www.ijg.org/files/jpegsrc.v8b.tar.gz
-# tar -xvf jpegsrc.v8b.tar.gz
-# cd jpeg-8b
-# ./configure --enable-shared=no --prefix=$PREFIX
-# make
-# make install
-# cd $PREFIX/dist
-# 
-# 
-# clear
-# echo -------------------------
-# echo "Downloading and building PNG"
-# echo -------------------------
-# curl -O ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.4.4.tar.gz
-# tar -xvf libpng-1.4.4.tar.gz
-# cd libpng-1.4.4
-# ./configure --enable-shared=no --prefix=$PREFIX
-# make
-# make install
-# cd $PREFIX/dist
-# 
-# 
-# clear
-# echo -------------------------
-# echo "Downloading and building TIFF"
-# echo -------------------------
-# ${WGET} ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.9.4.tar.gz
-# tar -xvf tiff-3.9.4.tar.gz
-# cd tiff-3.9.4
-# ./configure --enable-shared=no --prefix=$PREFIX
-# make && make install
-# cd $PREFIX/dist
 
 
 #test if cmake is installed.
 type -P cmake &>/dev/null || { 
-clear
-echo -------------------------
-echo "Checking out and building CMake"
-echo -------------------------
-${WGET} http://www.cmake.org/files/v2.8/cmake-2.8.2.tar.gz
-tar -xvf cmake-2.8.2.tar.gz
-cd cmake-2.8.2
-./configure --prefix=$PREFIX
-make && make install clean
-cd $PREFIX/dist;
+	clear
+	echo -------------------------
+	echo "Downloading and building CMake"
+	echo -------------------------
+	cd $PREFIX/dist
+	${WGET} http://www.cmake.org/files/v2.8/cmake-2.8.2.tar.gz
+	tar -xvf cmake-2.8.2.tar.gz
+	cd cmake-2.8.2
+	./configure --prefix=$PREFIX
+	make && make install clean
 }
 
 
@@ -240,20 +231,84 @@ cd $PREFIX/dist;
 #	the libraries no matter what, and it *might* cause problems later on to use a different version
 # 2. We can just suck it up and use the versions that come with OpenCV.  To do this, we need to
 #	not have these varialbes (which are required for FFMPEG to find x264, faac, etc.) set.
-export CFLAGS=""
-export CPPFLAGS=""
-export LDFLAGS=""
+
+if [ -e "$PREFIX/lib/libopencv_core.a" ]
+then
+	echo "OpenCV is already installed"
+else
+	export CFLAGS=""
+	export CPPFLAGS=""
+	export LDFLAGS=""
+	clear
+	echo -------------------------
+	echo "Checking out and building OpenCV"
+	echo -------------------------
+	cd $PREFIX/dist;
+	svn -r 4374 co https://code.ros.org/svn/opencv/trunk/opencv opencv2.2
+	cd opencv2.2
+	cmake -G "Unix Makefiles" -D CMAKE_OSX_ARCHITECTURES="i386;x86_64" -D BUILD_EXAMPLES=OFF -D BUILD_NEW_PYTHON_SUPPORT=OFF -D BUILD_TESTS=OFF -D BUILD_SHARED_LIBS=OFF -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=$PREFIX .
+	make && make install
+	export CFLAGS="-I$PREFIX/include"
+	export CPPFLAGS="-I$PREFIX/include"
+	export LDFLAGS="-L$PREFIX/lib"
+fi
+
+
+if [ -e "$PREFIX/include/gavl/gavl.h" ]
+then
+	echo "GAVL is already installed"
+else
+	clear
+	echo -------------------------
+	echo "Downloading and building gavl-1.1.1"
+	echo -------------------------
+	cd $PREFIX/dist
+	${WGET} http://downloads.sourceforge.net/project/gmerlin/gavl/1.1.1/gavl-1.1.1.tar.gz
+	tar -xvf gavl-1.1.1.tar.gz
+	cd $PREFIX/dist/gavl-1.1.1
+	./configure --prefix=$PREFIX --without-doxygen --disable-libpng
+	make
+	make install
+fi
+
+
+
+if [ -e "$PREFIX/include/frei0r.h" ]
+then
+	echo "Frei0r is already installed"
+else
+	clear
+	echo -------------------------
+	echo "Downloading and building frei0r-plugins-1.2"
+	echo -------------------------
+	export LDFLAGS="-L$PREFIX/lib -framework vecLib"
+	cd $PREFIX/dist
+	${WGET} http://www.piksel.no/frei0r/releases/frei0r-plugins-1.2.tar.gz
+	tar -xvf frei0r-plugins-1.2.tar.gz
+	cd $PREFIX/dist/frei0r-plugins-1.2
+	./configure --prefix=$PREFIX
+	# the 'make' will fail because of a problem with the documentation.  But the build did succeed
+	make --keep-going --ignore-errors
+	make install
+fi
+
+
 
 clear
 echo -------------------------
-echo "Checking out and building OpenCV"
+echo "Checking out and building FFMPEG"
 echo -------------------------
-svn -r 4374 co https://code.ros.org/svn/opencv/trunk/opencv opencv2.2
-cd opencv2.2
-#patch -p0 -i $PREFIX/share/patches/opencv_framehack_rev3713.patch
-cmake -G "Unix Makefiles" -D CMAKE_OSX_ARCHITECTURES="i386;x86_64" -D BUILD_EXAMPLES=OFF -D BUILD_NEW_PYTHON_SUPPORT=OFF -D BUILD_TESTS=OFF -D BUILD_SHARED_LIBS=OFF -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=$PREFIX .
-make && make install
 cd $PREFIX/dist
+svn co -r 26206 svn://svn.ffmpeg.org/ffmpeg/trunk ffmpeg 
+cd $PREFIX/dist/ffmpeg
+./configure --prefix=$PREFIX --enable-frei0r --disable-asm --disable-stripping --enable-debug --enable-gpl --disable-doc --enable-libmp3lame --enable-libx264 --enable-nonfree --enable-libvorbis --enable-libxvid --enable-version3 --enable-pthreads --enable-libfaac --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libtheora --enable-libxvid --enable-x11grab
+make && make install
+
+
+#ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.9.4.tar.gz
+#ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng-1.4.4.tar.gz
+#http://www.ijg.org/files/jpegsrc.v8b.tar.gz
+#http://www.ece.uvic.ca/~mdadams/jasper/software/jasper-1.900.1.zip
 
 
 
