d4070f558c965ee2550c742d1b453e4ada123c4f
Jeff Crouse <jefftimesten@gmail.com>
2011-04-05 16:48:29 -0400

    More work on logomunge.  Added zlib to compress the data once it has been assembled and saved out.  Still aren't using the corners, and the code is a mess.  I need to make some classes and neaten stuff up.

--- logomunge/src/main.cpp
+++ logomunge/src/main.cpp
@@ -7,6 +7,17 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
+#include "zlib.h"
+
+#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
+#  include <fcntl.h>
+#  include <io.h>
+#  define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
+#else
+#  define SET_BINARY_MODE(file)
+#endif
+
+#define CHUNK 16384
 
 using namespace std;
 using namespace cv;
@@ -30,6 +41,9 @@ void scan_directory(fs::path dir, int level);
 bool is_image(fs::path p);
 void generate_datafile(Mat image, fs::path data_path);
 void on_mouse( int event, int x, int y, int flags, void* param );
+void remove_query_string(fs::path& p);
+int def(FILE *source, FILE *dest, int level);
+void compress_data(fs::path ipath);
 
 // ----------------------------------------------------
 int main (int argc, char * const argv[])
@@ -86,13 +100,12 @@ int main (int argc, char * const argv[])
 		cout << "No directory provided.  Exiting." << endl;
 		return 0;
 	}
-	
+
 	
 	vector<string> directories = vm["directory"].as< vector<string> >();
 	for(int i=0; i<directories.size(); i++)
 	{
-		fs::path dir_path( directories[i] );
-		scan_directory(dir_path, 0);
+		scan_directory(fs::path( directories[i] ), 0);
 	}
 }
 
@@ -116,24 +129,16 @@ void scan_directory(fs::path dir, int level)
 	
 	cout << "Opening " << dir << endl;
 	
-	// This will hold descriptors for every image in the directory.
+	// This will hold descriptors and keypoints for every image in the directory.
 	vector<Mat> all_descriptors;
+	vector< vector<KeyPoint> > all_keypoints;
 	
 	// Loop through the contents of the directory and assemble SURF points of any images found.
 	fs::directory_iterator end_iter;
 	for (fs::directory_iterator dir_itr( dir ); dir_itr != end_iter;  ++dir_itr )
 	{
-		// If there is a query string after the extension, take it off!
 		fs::path file = dir_itr->path();
-		string old_name = file.string();
-		size_t found = old_name.find("&");
-		if (found!=string::npos)
-		{
-			string new_name = old_name.substr(0, found);
-			fs::rename(dir_itr->path(), new_name);
-			file = fs::path(new_name);
-		}
-
+		
 		
 		// Choose what to do with the the item in the iterator
 		if(fs::is_directory(file))
@@ -142,8 +147,10 @@ void scan_directory(fs::path dir, int level)
 		}
 		else if(is_image(file))
 		{
+			remove_query_string(file);
+			
 			// read in the image as a 3-channel Mat
-			Mat image = imread( file.string(), 1);
+			Mat image = imread( file.string(), 1 );
 			
 			fs::path cnrfile = file;
 			cnrfile.replace_extension(".cnr");
@@ -160,17 +167,16 @@ void scan_directory(fs::path dir, int level)
 			{
 				// TO DO:  Read in and use the corners from the file!
 				
-				
 				// examine the image.
 				vector<KeyPoint> keypoints;
 				Mat descriptors;
 				
 				cout << "Identifying keypoints" << endl;
 				detector.detect(image, keypoints);
+				all_keypoints.push_back( keypoints );
 				
 				cout << "Extracting descriptors " << endl;
 				extractor.compute(image, keypoints, descriptors);
-				
 				all_descriptors.push_back( descriptors );
 			}
 		}
@@ -188,23 +194,48 @@ void scan_directory(fs::path dir, int level)
 		
 		cout << "saving data to " << datafile << endl;
 		
-		FileStorage descriptors_fs;
-		descriptors_fs.open(datafile.string(), FileStorage::WRITE);
-		if(descriptors_fs.isOpened())
+		FileStorage fs;
+		fs.open(datafile.string(), FileStorage::WRITE);
+		
+		if(fs.isOpened())
 		{
+			fs << "total" << (int)all_descriptors.size();
+			fs << "logos" << "{";
+			
 			char nodename[255];
 			for(int i=0; i<all_descriptors.size(); i++)
 			{
-				sprintf(nodename, "descriptors_%d", i);
-				descriptors_fs << string(nodename) << all_descriptors[i];
-				
+				fs << "logo" << "{";
+					sprintf(nodename, "image_%d", i);
+					fs << "filename" << nodename;
+					write(fs, "keypoints", all_keypoints[i]);
+					fs << "descriptors" << all_descriptors[i];
+				fs << "}";
 			}
-			descriptors_fs.release();
+			
+			fs << "}";
+			fs.release();
+			compress_data(datafile);
 		}
 	}
 }
 
 
+
+// ----------------------------------------------------
+// If an image has a name like myimage.jpg?fooblah=boo, rename it myimage.jpg 
+void remove_query_string(fs::path& p)
+{
+	// If there is a query string after the extension, take it off!
+	string old_name = p.string();
+	size_t found = old_name.find("&");
+	if (found!=string::npos)
+	{
+		string new_name = old_name.substr(0, found);
+		fs::rename(old_name, new_name);
+		p = fs::path(new_name);
+	}	
+}
 
 
 // ----------------------------------------------------
@@ -296,4 +327,109 @@ void on_mouse( int event, int x, int y, int flags, void* param )
 		circle(image_w_margin, Point(x,y), 2, Scalar(0,0,255), 2);
 		corners.push_back(Point(x-margin, y-margin));
 	}
+}
+
+
+/* report a zlib or i/o error */
+void zerr(int ret)
+{
+    fputs("zpipe: ", stderr);
+    switch (ret) {
+		case Z_ERRNO:
+			if (ferror(stdin))
+				fputs("error reading stdin\n", stderr);
+			if (ferror(stdout))
+				fputs("error writing stdout\n", stderr);
+			break;
+		case Z_STREAM_ERROR:
+			fputs("invalid compression level\n", stderr);
+			break;
+		case Z_DATA_ERROR:
+			fputs("invalid or incomplete deflate data\n", stderr);
+			break;
+		case Z_MEM_ERROR:
+			fputs("out of memory\n", stderr);
+			break;
+		case Z_VERSION_ERROR:
+			fputs("zlib version mismatch!\n", stderr);
+    }
+}
+
+
+void compress_data(fs::path ipath)
+{
+	fs::path opath = ipath;
+	opath.replace_extension(".data");
+	
+	cout << "compressing " << ipath << " to " << opath << endl;
+	
+	FILE* in  = fopen(ipath.string().c_str(), "r");
+	FILE* out = fopen(opath.string().c_str(), "w+");
+	
+	int ret = def(in, out, Z_DEFAULT_COMPRESSION);
+	if (ret == Z_OK)
+	{
+		remove(ipath);
+	}
+	else
+	{
+		zerr(ret);
+	}
+}
+
+
+/* Compress from file source to file dest until EOF on source.
+ def() returns Z_OK on success, Z_MEM_ERROR if memory could not be
+ allocated for processing, Z_STREAM_ERROR if an invalid compression
+ level is supplied, Z_VERSION_ERROR if the version of zlib.h and the
+ version of the library linked do not match, or Z_ERRNO if there is
+ an error reading or writing the files. */
+int def(FILE *source, FILE *dest, int level)
+{
+    int ret, flush;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+	
+    /* allocate deflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    ret = deflateInit(&strm, level);
+    if (ret != Z_OK)
+        return ret;
+	
+    /* compress until end of file */
+    do {
+        strm.avail_in = fread(in, 1, CHUNK, source);
+        if (ferror(source)) {
+            (void)deflateEnd(&strm);
+            return Z_ERRNO;
+        }
+        flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
+        strm.next_in = in;
+		
+        /* run deflate() on input until output buffer not full, finish
+		 compression if all of source has been read in */
+        do {
+            strm.avail_out = CHUNK;
+            strm.next_out = out;
+            ret = deflate(&strm, flush);    /* no bad return value */
+            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+            have = CHUNK - strm.avail_out;
+            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                (void)deflateEnd(&strm);
+                return Z_ERRNO;
+            }
+        } while (strm.avail_out == 0);
+        assert(strm.avail_in == 0);     /* all input will be used */
+		
+        /* done when last data in file processed */
+    } while (flush != Z_FINISH);
+    assert(ret == Z_STREAM_END);        /* stream will be complete */
+	
+    /* clean up and return */
+    (void)deflateEnd(&strm);
+    return Z_OK;
 }
